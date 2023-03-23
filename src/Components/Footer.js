a6c7463da4ac9c9da274d3c4da2f6f549fcdbc3d
Kopnop <robin.raetze@web.de>
2023-03-23 18:21:10 +0100

    cleaned up boilerplate

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className='footer--div'>
+                <p>made by Kopnop</p>
+                <span><a href="https://github.com/Kopnop/freecodecamp/tree/main/frontend/my-calculator" target="_blank" rel="noreferrer"><p>view source code</p>
+                    <FontAwesomeIcon className="git--icon" icon={faGithub} /></a></span>
+            </div>
+        </footer>
+    )
+}
